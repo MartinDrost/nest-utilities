fbe1d06441cad8edb4c7b6ac5d0891941ab4732f
Martin Drost <info@martindrost.nl>
2019-02-13 15:43:08 +0100

    Refactored populate method and fixed deeppopulate issue for multiple attributes on the same layer

--- server/services/crud.abstract.service.ts
+++ server/services/crud.abstract.service.ts
@@ -133,58 +133,72 @@ export abstract class CrudService<IModel extends Document> {
     }
 
     // create deeppopulated options
-    const options = [] as ModelPopulateOptions[];
+    const options: ModelPopulateOptions[] = [];
     for (const path of paths) {
-      const subPaths = path.split(".");
-      const startingpoint = subPaths.shift();
-      const option = {
-        match,
-        path: startingpoint
-      } as ModelPopulateOptions;
-
-      let reference = option;
-      for (const subPath of subPaths) {
-        reference = reference.populate = {
-          match,
-          path: subPath
-        };
-      }
-      options.push(option);
+      this.addOptions(path, options, match);
     }
 
     return modelItem
       .populate(options)
       .execPopulate()
       .then(model => {
+        // change all undefined values to null
         for (const path of paths) {
-          let reference = model as any;
-          const subPaths = path.split(".");
-          const endpoint = subPaths.pop();
-          if (endpoint === undefined) {
-            continue;
-          }
-
-          // create a subpath reference if it exists
-          for (const subPath of subPaths) {
-            reference = reference[subPath];
-          }
-
-          // remove unpopulated ObjectID's
-          if (
-            Array.isArray(reference[endpoint]) ||
-            arrays.indexOf(path) !== -1
-          ) {
-            const array = _.flatten([reference]);
-            array.map(index => {
-              index[endpoint] = ((index[endpoint] as any[]) || []).filter(
-                item => item instanceof ObjectID === false
-              );
-            });
-          } else if (reference[endpoint] instanceof ObjectID === true) {
-            reference[endpoint] = null;
+          if (_.get(model, path) === null) {
+            _.set(model, path, null);
           }
         }
+
+        // remove unpopulated objectID's from populated arrays
+        for (const array of arrays) {
+          _.set(
+            model,
+            array,
+            _.get(model, array, []).filter(
+              item => item instanceof ObjectID === false
+            )
+          );
+        }
+
         return model;
       });
+  }
+
+  /**
+   * create populate options based on paths
+   * @param path
+   * @param layer
+   * @param match
+   */
+  private addOptions(
+    path: string,
+    layer: ModelPopulateOptions[],
+    match: any = { isRemoved: { $in: [false, null] } }
+  ): void {
+    // break the cycle if the path has ended
+    if (!path) {
+      return;
+    }
+
+    // separate the path in the current position and the journey ahead
+    const dismembered = path.split(".");
+    const position = dismembered.shift();
+    const journey = dismembered.join(".");
+
+    // check if the current position has already been mapped
+    for (let i = 0; i < layer.length; i++) {
+      if (layer[i].path === position) {
+        this.addOptions(journey, layer[i].populate as any, match);
+        return;
+      }
+    }
+
+    // add the position otherwise
+    layer.unshift({
+      match,
+      path: position,
+      populate: []
+    });
+    this.addOptions(journey, layer[0].populate as any, match);
   }
 }
