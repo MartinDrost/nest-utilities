7c03053626f3a8b95d1bcc7978362b665b48aedf
Martin Drost <info@martindrost.nl>
2022-08-24 13:46:24 +0200

    exception filter now honors http exceptions

--- source/filters/http-exception.filter.ts
+++ source/filters/http-exception.filter.ts
@@ -10,25 +10,27 @@ import { Response } from "express";
 export class HttpExceptionFilter implements ExceptionFilter {
   constructor(private showErrorDetails: boolean) {}
 
-  catch(exception: HttpException, host: ArgumentsHost) {
+  catch(exception: Error, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
-    const status = exception.getStatus?.() ?? 500;
-    const json: { status: number; message: string; stack?: string } = {
-      status,
-      message: exception.message,
+
+    // return the exception as is if we're dealing with a thrown http exception
+    if (exception instanceof HttpException) {
+      response.status(exception.getStatus()).json(exception.getResponse());
+      return;
+    }
+
+    // else define it as an internal server error
+    const json: { status: number; message: string | object; stack?: string } = {
+      status: 500,
+      message: "Internal server error",
     };
 
-    // hide internal server error messages when disabled
-    if (status === 500 && this.showErrorDetails === false) {
-      json.message = "Internal server error";
-    }
-
-    // add stack trace when enabled
-    if (exception.stack && this.showErrorDetails) {
+    // show the message and stack if we allow the handler to
+    if (this.showErrorDetails) {
+      json.message = exception.message;
       json.stack = exception.stack;
     }
-
-    response.status(status).json(json);
+    response.status(json.status).json(json);
   }
 }
