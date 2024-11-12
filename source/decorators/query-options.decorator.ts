2e2f1161a9a4af49a9b8117e807162b171b1651d
MartinDrost <info@martindrost.nl>
2024-11-12 12:35:01 +0100

    Add support for custom query options from request headers

--- source/decorators/query-options.decorator.ts
+++ source/decorators/query-options.decorator.ts
@@ -1,6 +1,5 @@
 import { createParamDecorator, ExecutionContext } from "@nestjs/common";
 import { Request, Response } from "express";
-import { IQueryOptions } from "fundering";
 import { IQueryOptionsConfig } from "../interfaces/query-options-config.interface";
 import { queryToOptions } from "../utilities/controller.utilities";
 
@@ -8,12 +7,22 @@ export const QueryOptions = createParamDecorator<IQueryOptionsConfig>(
   (config: IQueryOptionsConfig, ctx: ExecutionContext) => {
     const request: Request = ctx.switchToHttp().getRequest();
     const response: Response = ctx.switchToHttp().getResponse();
-    const options: IQueryOptions = {
+
+    if (request.header["x-query-options"]) {
+      return {
+        ...queryToOptions(
+          JSON.parse(request.header["x-query-options"]),
+          config
+        ),
+        request,
+        response,
+      };
+    }
+
+    return {
       ...queryToOptions(request.query, config),
       request,
       response,
     };
-
-    return options;
   }
 );
